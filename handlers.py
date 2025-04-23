0e549168b24c4cfe175283373c819799bc643abb
367yz <yurii.zaionts.knm.2019@lpnu.ua>
2025-04-23 11:02:44 +0300

    fix handlers

--- handlers.py
+++ handlers.py
@@ -1,6 +1,8 @@
 from aiogram import types
+from aiogram.fsm.context import FSMContext
 from config import PDF_PATH, CSV_PATH
 from keyboards import main_kb
+from states import ContactForm
 import csv
 import os
 
@@ -14,16 +16,16 @@ async def pdf_handler(message: types.Message):
     else:
         await message.answer("Вибачте, файл тимчасово недоступний.")
 
-async def request_contact(message: types.Message, state):
+async def request_contact(message: types.Message, state: FSMContext):
     await message.answer("Будь ласка, введіть ваше ім'я:")
-    await state.set_state("name")
+    await state.set_state(ContactForm.name)
 
-async def contact_name(message: types.Message, state):
+async def contact_name(message: types.Message, state: FSMContext):
     await state.update_data(name=message.text)
     await message.answer("Ваш номер телефону:")
-    await state.set_state("phone")
+    await state.set_state(ContactForm.phone)
 
-async def contact_phone(message: types.Message, state):
+async def contact_phone(message: types.Message, state: FSMContext):
     data = await state.get_data()
     name = data.get("name")
     phone = message.text
